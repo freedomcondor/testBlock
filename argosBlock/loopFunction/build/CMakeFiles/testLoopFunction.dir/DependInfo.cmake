ca2aa941b302462f08f8f872cdd9b84964f62e23
freedomcondor <zhuweixu_harry@126.com>
2017-11-07 11:59:32 +0100

    prepare to box

---
+++ argosBlock/loopFunction/build/CMakeFiles/testLoopFunction.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/harry/testBlock/argosBlock/loopFunction/testLoopFunction.cpp" "/home/harry/testBlock/argosBlock/loopFunction/build/CMakeFiles/testLoopFunction.dir/testLoopFunction.cpp.o"
+  "/home/harry/testBlock/argosBlock/loopFunction/testbench.cpp" "/home/harry/testBlock/argosBlock/loopFunction/build/CMakeFiles/testLoopFunction.dir/testbench.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/usr/include/opencv"
+  "/usr/local/include/apriltag"
+  "/usr/include/lua5.2"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
--- lua/func.lua
+++ lua/func.lua
@@ -26,8 +26,25 @@ package.path = package.path ..';../lua/?.lua'		-- for opengl testbench
 package.path = package.path ..';../../lua/?.lua'	-- for argos testbench
 --package.path = package.path ..';loopFunction/?.lua'
 require("calcTagPos")
+Vec3 = require("Vector3")
 
 function func(tagList)
+	--[[
+	-- tagList has:
+		{
+			n
+			1 = {center = {x,y}
+				 corners = {1 = {x,y}
+							2 = {x,y}
+							3 = {x,y}
+							4 = {x,y}
+							}
+				}
+			2 = {}
+			3 = {}
+		}
+	--]]
+
 	--[[ -- for testing tagList
 	print(a.timestamp)
 	print(a.n)
@@ -43,19 +60,33 @@ function func(tagList)
 	end
 	--]]
 
+	local halfL = 5
+
+	-- Calc position of tags ------------------------------------
 	--print("tagList got:",tagList.n,"tags")
-	pos = {n = tagList.n}
+	local pos = {n = tagList.n}
 	for i = 1, tagList.n do
 		--print("\tfor the",i,"tag")
 
-		tagList[i].halfL = 5
+		tagList[i].halfL = halfL
 		pos[i] = calTagPos(tagList[i])
 			-- calTagPos returns a table (for each tag)
-				-- {rotation = {x=,y=,z=}  the direction vector of the tag
-				--	translation = {x=,y=,z=} the position of the the tag
-				--	quaternion = {x,y,z,w}  the quaternion rotation of the tag
+				-- {rotation = {x=,y=,z=}  <a vector> the direction vector of the tag
+				--	translation = {x=,y=,z=} <a vector> the position of the the tag
+				--	quaternion = {x,y,z,w} <a quaternion> the quaternion rotation of the tag
 				-- }
 	end
+
+	-- Calc boxes --------- ------------------------------------
+
+	set = {n = tagList.n}
+	for i = 1, tagList.n do
+		set[i] = {}
+		set[i].translation = pos[i].translation + halfL * pos[i].rotation:nor()
+		set[i].rotation = pos[i].rotation
+		set[i].quaternion = pos[i].quaternion
+	end
+
 
 	--[[ -- for testing pos(calTagPos returns)
 	print(pos.n)
@@ -66,6 +97,7 @@ function func(tagList)
 	--]]
 
 	return pos
+	--return set
 	--[[
 		-- pos have
 		pos.n
