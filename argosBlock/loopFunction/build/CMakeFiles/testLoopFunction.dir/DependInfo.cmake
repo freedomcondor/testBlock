b7b5aaa929f9a816da929ca253229bdf55b6f31c
freedomcondor <freedomcondor@126.com>
2017-11-07 12:13:04 +0100

    try MacOS but failed

--- argosBlock/loopFunction/build/CMakeFiles/testLoopFunction.dir/DependInfo.cmake
+++ argosBlock/loopFunction/build/CMakeFiles/testLoopFunction.dir/DependInfo.cmake
@@ -4,16 +4,16 @@ set(CMAKE_DEPENDS_LANGUAGES
   )
 # The set of files for implicit dependencies of each language:
 set(CMAKE_DEPENDS_CHECK_CXX
-  "/home/harry/testBlock/argosBlock/loopFunction/testLoopFunction.cpp" "/home/harry/testBlock/argosBlock/loopFunction/build/CMakeFiles/testLoopFunction.dir/testLoopFunction.cpp.o"
-  "/home/harry/testBlock/argosBlock/loopFunction/testbench.cpp" "/home/harry/testBlock/argosBlock/loopFunction/build/CMakeFiles/testLoopFunction.dir/testbench.cpp.o"
+  "/Users/harry/Desktop/testBlock/argosBlock/loopFunction/testLoopFunction.cpp" "/Users/harry/Desktop/testBlock/argosBlock/loopFunction/build/CMakeFiles/testLoopFunction.dir/testLoopFunction.cpp.o"
+  "/Users/harry/Desktop/testBlock/argosBlock/loopFunction/testbench.cpp" "/Users/harry/Desktop/testBlock/argosBlock/loopFunction/build/CMakeFiles/testLoopFunction.dir/testbench.cpp.o"
   )
-set(CMAKE_CXX_COMPILER_ID "GNU")
+set(CMAKE_CXX_COMPILER_ID "Clang")
 
 # The include file search paths:
 set(CMAKE_CXX_TARGET_INCLUDE_PATH
-  "/usr/include/opencv"
+  "/usr/local/include"
+  "/usr/local/include/opencv"
   "/usr/local/include/apriltag"
-  "/usr/include/lua5.2"
   )
 
 # Targets to which this target links.
