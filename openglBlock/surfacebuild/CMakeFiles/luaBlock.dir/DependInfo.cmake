b5983b1434470b5eb73f8edb0bf6db0d45d0d7a3
freedomcondor <freedomcondor@126.com>
2017-12-05 01:30:00 +0100

    surfacebuild

---
+++ openglBlock/surfacebuild/CMakeFiles/luaBlock.dir/DependInfo.cmake
@@ -0,0 +1,25 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/mnt/c/Users/freed/Desktop/testBlock/openglBlock/debug_opengl.cpp" "/mnt/c/Users/freed/Desktop/testBlock/openglBlock/surfacebuild/CMakeFiles/luaBlock.dir/debug_opengl.cpp.o"
+  "/mnt/c/Users/freed/Desktop/testBlock/openglBlock/function_opengl.cpp" "/mnt/c/Users/freed/Desktop/testBlock/openglBlock/surfacebuild/CMakeFiles/luaBlock.dir/function_opengl.cpp.o"
+  "/mnt/c/Users/freed/Desktop/testBlock/testbench/testbench.cpp" "/mnt/c/Users/freed/Desktop/testBlock/openglBlock/surfacebuild/CMakeFiles/luaBlock.dir/mnt/c/Users/freed/Desktop/testBlock/testbench/testbench.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/usr/include/opencv"
+  "/usr/local/include/apriltag"
+  "/usr/include/lua5.2"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
