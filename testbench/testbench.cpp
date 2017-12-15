4a3d63709fdbcb9027ae1a303e0ebffb2486e33d
freedomcondor <zhuweixu_harry@126.com>
2017-12-15 17:36:48 +0100

    opencv data

--- testbench/testbench.cpp
+++ testbench/testbench.cpp
@@ -47,11 +47,11 @@ int testbench_init(int SystemWeight, int SystemHeight)
 	//printf("init\n");
 	L = luaL_newstate();
 	luaL_openlibs(L);
-	//if ((luaL_loadfile(L,"../lua/func.lua")) || (lua_pcall(L,0,0,0)))
-	if ((luaL_loadfile(L,"../lua/func_fordata.lua")) || (lua_pcall(L,0,0,0)))
+	if ((luaL_loadfile(L,"../lua/func.lua")) || (lua_pcall(L,0,0,0)))
+	//if ((luaL_loadfile(L,"../lua/func_fordata.lua")) || (lua_pcall(L,0,0,0)))
 	{
-		//if ((luaL_loadfile(L,"../../lua/func.lua")) || (lua_pcall(L,0,0,0)))
-		if ((luaL_loadfile(L,"../../lua/func_fordata.lua")) || (lua_pcall(L,0,0,0)))
+		if ((luaL_loadfile(L,"../../lua/func.lua")) || (lua_pcall(L,0,0,0)))
+		//if ((luaL_loadfile(L,"../../lua/func_fordata.lua")) || (lua_pcall(L,0,0,0)))
 		//if ((luaL_loadfile(L,"../../lua/debugger.lua")) || (lua_pcall(L,0,0,0)))
 			{printf("open lua file fail : %s\n",lua_tostring(L,-1));return -1;}
 	}
@@ -75,7 +75,7 @@ int testbench_init(int SystemWeight, int SystemHeight)
 	apriltag_detector_add_family(m_psTagDetector, m_psTagFamily);
 
 	camera_flag = 1;
-	record_flag = 1;
+	//record_flag = 1;
 
 	if (camera_flag == 1)
 	{
